5c81bba658b22c145c8a533260d5c9d2f01ecc83
Jure Zmrzlikar <zmrzlikar.jure@gmail.com>
2020-12-06 08:50:01 +0100

    Day 5

---
+++ day05/day_05_2.py
@@ -0,0 +1,27 @@
+def bisect(string):
+    min = 0
+    max = 2 ** len(string)
+    for letter in string:
+        if letter in ["F", "L"]:
+            max = min + (max - min) / 2
+        if letter in ["B", "R"]:
+            min = max - (max - min) / 2
+        # print(letter, max, min)
+
+    return min
+
+def get_pass_id(string):
+    row = bisect(string[:7])
+    column = bisect(string[7:])
+    return (row, column, row * 8 + column)
+
+
+pids = []
+with open("input.txt") as handle:
+    for line in handle:
+        _, _, pid = get_pass_id(line.strip())
+        pids.append(pid)
+
+for i in range(1, 1023):
+    if i not in pids and i+1 in pids and i-1 in pids:
+        print(i)
