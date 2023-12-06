30e7ec7fe6fbe16718d69eef746ad759e5358117
JureZmzrlikar <zmrzlikar.jure@gmail.com>
2023-12-06 11:14:57 +0100

    AoC 2023 Day 6

---
+++ 2023/day06_2.py
@@ -0,0 +1,36 @@
+def compute_distances(time):
+    distances = []
+    for hold_time in range(time + 1):
+        speed = hold_time
+        distance = speed * (time - hold_time)
+        distances.append(distance)
+
+    return distances
+
+
+with open("input.txt") as handle:
+    times = handle.readline().strip().split(":")[1].strip().split(" ")
+    distances = handle.readline().strip().split(":")[1].strip().split(" ")
+
+# Remove empty strings
+times = [e for e in times if e != ""]
+distances = [e for e in distances if e != ""]
+
+# For part#2, just merge this into one single number
+times = [int("".join(times))]
+distances = [int("".join(distances))]
+
+prod = []
+for time, distance in zip(times, distances):
+    my_distances = compute_distances(time)
+    ways_to_win = 0
+    for my_dist in my_distances:
+        if my_dist > distance:
+            ways_to_win += 1
+
+    prod.append(ways_to_win)
+
+final = 1
+for p in prod:
+    final *= p
+print(final)
